9e175e2ab79c6c306a7f3805e573b99953613d11
chandrakant <chandrakantthakur817@gmail.com>
2025-01-05 19:43:40 +0530

    Project Setup done

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 from typing import List
 
 PROJECT_NAME = "MLPipeline"
-VERSION = "0.0.1"
+VERSION = "0.0.0"
 DESCRIPTION = "This is Machine Learning Project"
 AUTHOR = "Chandrakant Thakur"
 AUTHOR_EMAIL = "chandrakantthakur817@gmail.com"
--- src/data_ingestion.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib as plt
-import os
-from sklearn.model_selection import train_test_split
-import yaml
-
-
-
